24ad9d05caf6a2fd77872fb7f62604bf203b4539
κασσάνδρα.eth <0xDADA@protonmail.com>
2025-08-13 11:02:08 +0200

    feat: add shield helpers to RailgunAccount (#1)
    * feat: add shield helpers to RailgunAccount
    
    * fix: use keccak256 for shieldPrivateKey

--- src/wallet-utils/railgun-account.ts
+++ src/wallet-utils/railgun-account.ts
@@ -1,5 +1,13 @@
 import { deriveNodes, WalletNode } from '../railgun-lib/key-derivation/wallet-node';
-import { encodeAddress, /*decodeAddress*/ } from '../railgun-lib/key-derivation/bech32';
+import { encodeAddress } from '../railgun-lib/key-derivation/bech32';
+import { Mnemonic } from '../railgun-lib/key-derivation/bip39';
+import { JsonRpcProvider, Wallet } from 'ethers';
+import { ShieldNoteERC20 } from '../railgun-lib/note/erc20/shield-note-erc20';
+import { ByteUtils } from '../railgun-lib/utils/bytes';
+import { ShieldRequestStruct } from '../railgun-lib/abi/typechain/RailgunSmartWallet';
+import { getSharedSymmetricKey } from '../railgun-lib/utils/keys-utils';
+import { hexToBytes } from 'ethereum-cryptography/utils';
+import { keccak256 } from 'ethereum-cryptography/keccak';
 
 const ACCOUNT_VERSION = 1;
 const ACCOUNT_CHAIN_ID = undefined;
@@ -8,20 +16,20 @@ export default class RailgunAccount {
 
   private spending: WalletNode;
   private viewing: WalletNode;
+  private ethSigner: Wallet;
 
-  constructor(mnemonic: string, accountIndex: number) {
+  constructor(mnemonic: string, accountIndex: number, rpcUrl: string) {
     const {spending, viewing} = deriveNodes(mnemonic, accountIndex);
     this.spending = spending;
     this.viewing = viewing;
+    const provider = new JsonRpcProvider(rpcUrl);
+    this.ethSigner = new Wallet(Mnemonic.to0xPrivateKey(mnemonic, accountIndex), provider);
   }
 
-
-  async getAddress() {
-    const {pubkey: spendingPubkey} = this.spending.getSpendingKeyPair();
+  async getRailgunAddress(): Promise<string> {
     const {pubkey: viewingPubkey} = await this.viewing.getViewingKeyPair();
-    const nullifyingKey = await this.viewing.getNullifyingKey();
-  
-    const masterPubkey = WalletNode.getMasterPublicKey(spendingPubkey, nullifyingKey);
+    const masterPubkey = await this.getMasterPublicKey();
+    
     const address = encodeAddress({
       masterPublicKey: masterPubkey,
       viewingPublicKey: viewingPubkey,
@@ -30,5 +38,48 @@ export default class RailgunAccount {
     });
 
     return address;
+  }
+
+  async getMasterPublicKey(): Promise<bigint> {
+    const {pubkey: spendingPubkey} = this.spending.getSpendingKeyPair();
+    const nullifyingKey = await this.viewing.getNullifyingKey();
+    return WalletNode.getMasterPublicKey(spendingPubkey, nullifyingKey);
+  }
+
+  async getShieldPrivateKey(): Promise<Uint8Array> {
+    // TODO: this is made up, need to find canonical way.
+    const msg = ShieldNoteERC20.getShieldPrivateKeySignatureMessage();
+    const signature = await this.ethSigner.signMessage(msg);
+    const signatureBytes = ByteUtils.hexStringToBytes(signature);
+    return keccak256(signatureBytes);
+  }
+
+  async decryptShieldNoteRandomness(shieldNote: ShieldRequestStruct): Promise<string> {
+    const bundle = shieldNote.ciphertext.encryptedBundle.map(String) as [string, string, string];
+    const shieldKey = String(shieldNote.ciphertext.shieldKey);
+    const {privateKey: viewingPrivateKey} = await this.viewing.getViewingKeyPair();
+
+    const sharedKey = await getSharedSymmetricKey(viewingPrivateKey, hexToBytes(shieldKey));
+    if (!sharedKey) {
+      throw new Error('Failed to get shared key');
+    }
+    return ShieldNoteERC20.decryptRandom(bundle, sharedKey);
+  }
+
+  async getEncodedShieldNote(token: string, value: bigint, random: string): Promise<ShieldRequestStruct> {
+    const shieldNote = await this.getShieldNote(token, value, random);
+    const request = await this.encodeShieldNote(shieldNote);
+    return request;
+  }
+
+  async getShieldNote(token: string, value: bigint, random: string): Promise<ShieldNoteERC20> {
+    const masterPubkey = await this.getMasterPublicKey();
+    return new ShieldNoteERC20(masterPubkey, random, value, token);
+  }
+
+  async encodeShieldNote(shieldNote: ShieldNoteERC20): Promise<ShieldRequestStruct> {
+    const shieldPrivateKey = await this.getShieldPrivateKey();
+    const {pubkey: viewingPubkey} = await this.viewing.getViewingKeyPair();
+    return shieldNote.serialize(shieldPrivateKey, viewingPubkey);
   }
 }
