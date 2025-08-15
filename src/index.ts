060a769da8873a9798b6f3076935cc60cb18075c
κασσάνδρα.eth <0xDADA@protonmail.com>
2025-08-15 10:12:23 +0200

    feat: add ways to init RailgunAccount (#2)

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 
 // Load environment variables from .env file
 dotenv.config();
-const RPC_URL = process.env.RPC_URL || 'https://rpc.ankr.com/sepolia';
 const MNEMONIC = process.env.MNEMONIC || 'test test test test test test test test test test test test';
 const ACCOUNT_INDEX = Number(process.env.ACCOUNT_INDEX) || 0;
 
@@ -12,7 +11,7 @@ const TOKEN = '0x97a36608DA67AF0A79e50cb6343f86F340B3b49e';
 const VALUE = 10000000000000n;
 
 async function main() {
-  const railgunAccount = new RailgunAccount(MNEMONIC, ACCOUNT_INDEX, RPC_URL);
+  const railgunAccount = RailgunAccount.fromMnemonic(MNEMONIC, ACCOUNT_INDEX);
   const zkAddress = await railgunAccount.getRailgunAddress();
 
   console.log('0zk address:', zkAddress);
